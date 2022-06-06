1d5f33d2307ccb717ad98342592afcdb73737b95
Kevin <kevin@nedellec.eu>
2022-06-07 00:01:15 +0200

    Feat: Trending Section

---
+++ src/components/TrendingSection.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="trending">
+    <h2 class="trending__title">Trending</h2>
+
+    <ul class="trending__videos">
+      <li class="trending__video" :style="{backgroundImage: `url('@/assets/images/thumbnails/beyond-earth/trending')`}"></li>
+      <li class="trending__video"></li>
+      <li class="trending__video"></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .trending {
+    &__title {
+      font-size: 1.25rem;
+    }
+
+    &__videos {
+      display: flex;
+      gap: 1rem;
+      // overflow-x: scroll;
+    }
+
+    &__video { 
+      flex: 0 0 15rem;
+      height: 8.75rem;
+      border: 1px solid red;
+      border-radius: 0.5rem 0.5rem 0 0;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import SearchBar from "../components/SearchBar.vue";
+import TrendingSection from "../components/TrendingSection.vue";
 
 </script>
 
 <template>
   <SearchBar placeholder="Search for movies or TV series"/>
+  <TrendingSection />
 </template>
+
+<style lang="scss">
+  .trending {
+    margin-top: 2rem;
+  }
+</style>
